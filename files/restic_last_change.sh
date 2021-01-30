b3fc4dfe92eac98fe2d1e21f6a776fafc6314f25
Stefan Horst <stefan@ultrachaos.de>
2021-01-30 17:06:12 +0100

    report seconds not days

--- files/restic_last_change.sh
+++ files/restic_last_change.sh
@@ -5,13 +5,14 @@ folder=$1
 last_file=$(ls -tr $folder/index | tail -n 1)
 ftime=$(stat -c %Y $folder/index/$last_file)
 ctime=$(date +%s)
-diff=$(( (ctime - ftime) / 86400 ))
+diff=$(( ctime - ftime ))
+diff_days=$(( diff / 86400 ))
 
 count_index=$(ls -tr $folder/index | wc -l)
 count_locks=$(ls -tr $folder/locks | wc -l)
 count_keys=$(ls -tr $folder/keys | wc -l)
 count_snapshots=$(ls -tr $folder/snapshots | wc -l)
 
-
-echo "P restic_last_backup age=$diff;2;7 The last Restic Backup was $diff Days ago"
-echo "P restic_count index=$count_index|locks=$count_locks;1;2|keys=$count_keys|snapshots=$count_snapshots The Restic Backup contains $count_index Index Files, $count_locks Locks, $count_keys Keys and $count_snapshots Snapshots"
+# 172800s == 2d; 604800s == 7d
+echo "P restic_last_backup age=$diff;172800;604800 The last Restic Backup was $diff_days Days ago"
+echo "P restic_count index=$count_index|locks=$count_locks;2;5|keys=$count_keys|snapshots=$count_snapshots The Restic Backup contains $count_index Index Files, $count_locks Locks, $count_keys Keys and $count_snapshots Snapshots"
